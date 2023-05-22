1529b09a1449849510724a1042fe6a6dd24c250b
amorais- <amorais-@student.42lisboa.com>
2023-05-22 10:07:21 +0100

    CPP02

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -2,14 +2,48 @@
 
 Fixed::Fixed()
 {
-	number_value = 0;
 	std::cout << "Default constructor called" << std::endl;
+
+	int	i = 0;
+	number_value = 0;
+	mult_factor = 1;
+	while (i++ < fractional)
+		mult_factor *= 2;
 }
 
 Fixed::Fixed(const Fixed &n)
 {
 	std::cout << "Copy constructor called" << std::endl;
+
+	int	i = 0;
 	number_value = n.getRawBits();
+	mult_factor = 1;
+	while (i++ < fractional)
+		mult_factor *= 2;
+}
+
+Fixed::Fixed(const int n)
+{
+	std::cout << "Int constructor called" << std::endl;
+
+	int	i = 0;
+	mult_factor = 1;
+	while (i++ < fractional)
+		mult_factor *= 2;
+	// por protecao overflow
+	number_value = n * mult_factor;
+}
+
+Fixed::Fixed(const float n)
+{
+	std::cout << "Float constructor called" << std::endl;
+
+	int	i = 0;
+	mult_factor = 1;
+	while (i++ < fractional)
+		mult_factor *= 2;
+	number_value = n * mult_factor;
+	number_value = (int)roundf(number_value);
 }
 
 int Fixed::getRawBits() const
@@ -33,4 +67,14 @@ Fixed &Fixed::operator=(const Fixed &n)
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+float	Fixed::toFloat() const
+{
+	return ((float) number_value / mult_factor);
+}
+
+int	Fixed::toInt() const
+{
+	return ((int) roundf(number_value / mult_factor));
 }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -2,19 +2,25 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class	Fixed
 {
 	int	number_value;
 	static const int	fractional = 8;
+	int	mult_factor;
 
 public:
 	Fixed();
 	Fixed(const Fixed &n);
+	Fixed(const int n);
+	Fixed(const float n);
 	~Fixed();
 	Fixed & operator = (const Fixed &n);
 	int	getRawBits(void) const;
 	void	setRawBits(int const raw);
+	float	toFloat(void) const;
+	int		toInt(void) const;
 };
 
 #endif
